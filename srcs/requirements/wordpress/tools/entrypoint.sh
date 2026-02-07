b536d4dbae766162072ef401abdb33413fec8f36
parden <parden@42.inception.VM>
2026-02-07 19:43:43 +0100

    nginx + wp

---
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+mkdir -p $WEB_DIR
+cd $WEB_DIR
+rm -rf *
+
+wp core download --allow-root
+mv wp-config-sample.php wp-config.php
+
+wp config set DB_NAME $DB --allow-root
+wp config set DB_USER $DB_USR --allow-root
+wp config set DB_PASSWORD $DB_PWD --allow-root
+
+wp core install \
+    --url=$DOMAIN \
+    --title="$WP_TITLE" \
+    --admin_user=$WP_ADMIN_USR \
+    --admin_password=$WP_ADMIN_PWD \
+    --admin_email=$WP_ADMIN_EMAIL \
+    --skip-email \
+    --allow-root
+
+wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
+
+wp theme install astra --activate --allow-root
+wp plugin install redis-cache --activate --allow-root
+wp plugin update --all --allow-root
+
+wp redis enable --allow-root
+
+exec php-fpm -F
