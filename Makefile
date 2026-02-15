4bb34586aa7b2de73e53034fdc21499d6cfffe59
parden <parden@42.inception.VM>
2026-02-15 15:15:27 +0100

    db install

--- Makefile
+++ Makefile
@@ -1,15 +1,23 @@
-YAML=srcs/docker-compose.yml
-all:
+NAME := Inception
+
+YAML := srcs/docker-compose.yml
+
+all: $(NAME)
+
+$(NAME):
 	docker compose -f $(YAML) up
-wp:
-	docker compose -f $(YAML) build --no-cache wordpress
-db:
-	docker compose -f $(YAML) build --no-cache mariadb
-nginx:
-	docker compose -f $(YAML) build --no-cache nginx
-fresh:
-	wp
-	db
-	nginx
+
 clean:
-	docker rm -f webpress nginx mariadb
+	docker compose -f $(YAML) down
+re: clean all
+
+fclean:
+	-docker stop $$(docker ps -qa)
+	-docker rm $$(docker ps -qa)
+	-docker rmi -f $$(docker images -qa)
+	-docker volume rm $$(docker volume ls -q)
+	-docker network rm $$(docker network ls -q) 2>/dev/null
+fre: fclean all
+
+
+.PHONY: all clean fclean re fre
--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,20 +1,27 @@
 #!/bin/sh
 
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-    mariadbd --initialize-insecure --user=mysql --datadir=/var/lib/mysql
-    mariadbd --user=mysql --datadir=/var/lib/mysql --skip-networking &
-    MYSQL_PID=$!
-    sleep 2
-    mysql -u root <<EOF
-CREATE DATABASE IF NOT EXISTS $DB;
-CREATE USER IF NOT EXISTS '$DB_USR'@'%' IDENTIFIED BY '$DB_PWD';
-GRANT ALL PRIVILEGES ON $DB.* TO '$DB_USR'@'%';
+mariadb-install-db --user=mysql --datadir=/var/lib/mysql
+
+mariadbd --user=mysql --skip-networking &
+pid="$!"
+
+# Wait until MariaDB is ready
+until mariadb-admin ping --silent; do
+    sleep 1
+done
+
+# Create database and user
+mariadb -u root <<EOF
+CREATE DATABASE IF NOT EXISTS ${DB};
+CREATE USER IF NOT EXISTS '${DB_USR}'@'%' IDENTIFIED BY '${DB_PWD}';
+GRANT ALL PRIVILEGES ON ${DB}.* TO '${DB_USR}'@'%';
 ALTER USER 'root'@'localhost' IDENTIFIED BY '12345';
 FLUSH PRIVILEGES;
 EOF
 
-    kill $MYSQL_PID
-    wait $MYSQL_PID
-fi
+# Stop background server
+kill "$pid"
+wait "$pid"
 
-exec mariadbd --user=mysql --datadir=/var/lib/mysql
+# Start MariaDB in foreground (proper Docker behavior)
+exec mariadbd --user=mysql
